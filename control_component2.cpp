fb9f215a77939ebfc993a040771ed60a24e66782
bdebruijn <bdebruijn@luminize.nl>
2018-03-15 16:08:41 +0100

    add control_component2 singleton

---
+++ control_component2.cpp
@@ -0,0 +1,102 @@
+/************************************************************************
+ * Demo of SMP_SAFE instantiated component in C++ class
+ * 
+ * ArcEye 2015  <arceyeATmgwareDOTcoDOTuk>
+ * 
+ * Adapted for use in machinekit_ros_control
+ * Taken from src/hal/icomp-example/C%2B%2B_instantiated_comps/const-rtv2.cc
+ * https://github.com/machinekit/machinekit/blob/master/src/hal/icomp-example/C%2B%2B_instantiated_comps/const-rtv2.cc
+************************************************************************/ 
+
+
+#include <control_component2.h>
+
+namespace machinekit_ros_control
+{
+
+int ControlComponent2::comp_id = 0;
+ControlComponent2 *cr;
+
+struct inst_data
+    {
+    hal_float_t *out;
+    hal_float_t *value;
+    int local_argc;
+    const char **local_argv;
+    };
+
+ControlComponent2::ControlComponent2()
+{
+    int retval = 0;
+    cname = "control_component2";
+    comp_id = hal_init(cname);
+    if (comp_id < 0)
+        {
+        hal_print_msg(RTAPI_MSG_DBG,"Component creation ABORTED");
+        exit(-1);
+        }
+    
+    // allocate memory
+    if ((inst = (inst_data *) hal_malloc(sizeof(inst_data))) == NULL) {
+	    hal_print_msg(RTAPI_MSG_ERR,
+			    "%s: ERROR: hal_malloc failed\n", cname);
+        return;
+    }
+
+    // export pins and functions
+	if ((retval = export_controlcomponent2(inst, cname))) {
+	    hal_print_msg(RTAPI_MSG_ERR,
+			    "%s: ERROR: export_controlcomponent2 failed\n", cname);
+	    hal_exit(comp_id);
+	    return;
+	}
+
+    hal_ready(comp_id);
+}
+
+ControlComponent2::~ControlComponent2()
+{
+    hal_exit(comp_id);
+}
+
+int ControlComponent2::export_controlcomponent2(struct inst_data *ip, const char *name)
+{
+    int retval = 0;
+    
+    // create pins
+    if (((retval = hal_pin_float_newf(HAL_IN, &(ip->value), comp_id,
+                "%s.value",  name))) ||
+	    ((retval = hal_pin_float_newf(HAL_OUT, &(ip->out), comp_id,
+				    "%s.out", name))))
+    {
+	    return retval;
+    }
+
+    // export the function
+    if ((retval = hal_export_functf(dummy_funct,
+                ip, 1, 0, comp_id, "%s.dummy_funct", name)) < 0) {
+	return retval;
+    }
+
+    return retval;
+}
+
+void ControlComponent2::dummy_funct(void *arg, long period)
+{
+    struct inst_data *ip  = (inst_data *) arg;
+    *(ip->out) = *(ip->value);
+}
+
+extern "C" int rtapi_app_main(void)
+{
+    cr = new ControlComponent2();
+    return 0;
+}
+
+
+extern "C" void rtapi_app_exit(void)
+{
+    delete(cr);
+}
+
+}  // namespace
---
+++ control_component2.h
@@ -0,0 +1,72 @@
+/************************************************************************
+ * Demo of SMP_SAFE instantiated component in C++ class
+ * 
+ * ArcEye 2015  <arceyeATmgwareDOTcoDOTuk>
+ * 
+ * Adapted for use in machinekit_ros_control
+ * Taken from src/hal/icomp-example/C%2B%2B_instantiated_comps/const-rtv2.cc
+ * https://github.com/machinekit/machinekit/blob/master/src/hal/icomp-example/C%2B%2B_instantiated_comps/const-rtv2.cc
+************************************************************************/ 
+
+#ifndef CONTROL_COMPONENT_2_H
+#define CONTROL_COMPONENT_2_H
+
+#define _CORRECT_ISO_CPP_STRING_H_PROTO
+
+// Std C / C++
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>   /* Standard types */
+
+#include <rtapi.h>
+#include "rtapi_app.h"
+#include "rtapi_string.h"
+#include <hal.h>
+#include <hal_priv.h>
+#include <hal_accessor.h>
+#include "hal_internal.h"
+
+// Can't rely on bool working otherwise
+#undef TRUE
+#define TRUE (1)
+#undef FALSE
+#define FALSE (0)
+#undef true
+#define true (1)
+#undef false
+#define false (0)
+
+#ifdef MODULE_INFO
+MODULE_INFO(machinekit, "component:constant:Use a parameter to set the value of a pin");
+MODULE_INFO(machinekit, "pin:out:float:None:out::None");
+MODULE_INFO(machinekit, "pin:value:float:None:io::None");
+MODULE_INFO(machinekit, "funct:_:1:");
+MODULE_INFO(machinekit, "license:GPL");
+MODULE_LICENSE("GPL");
+#endif // MODULE_INFO
+
+RTAPI_TAG(HAL,HC_SMP_SAFE);
+
+namespace machinekit_ros_control
+{
+
+struct inst_data;
+
+class ControlComponent2
+{
+public:
+    ControlComponent2();
+    ~ControlComponent2();
+    
+    static int export_controlcomponent2(struct inst_data *ip, const char *name);
+    static void dummy_funct(void *arg, long period);
+
+    struct inst_data *inst;
+    static int comp_id;
+    const char *cname;
+};
+
+}  // namespace
+
+#endif
